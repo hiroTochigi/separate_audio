57ae19390f90e574c028a8b62d7cc73de0a04892
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2021-12-06 21:28:52 -0600

    clean up

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import time
 
 start = time.time()
 subprocess.run(['docker', 'exec', '-it', 'separate_audio', 'python', 'analyze/src/separate_audio.py'])
-#subprocess.run(['docker', 'exec', '-it', 'transcript_audio', 'python', 'analyze/src/transcript.py'])
-
 end = time.time()
 
 print(end - start)
